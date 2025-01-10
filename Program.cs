b2586294418a76df642b4155a79f52f778ec7569
Dave <dave.snz@hotmail.com>
2025-01-10 17:11:45 -0600

    Added code to call chipsecuritysystem and list the chip sequence

--- Program.cs
+++ Program.cs
@@ -10,6 +10,32 @@ namespace ChipSecuritySystem
     {
         static void Main(string[] args)
         {
+            List<ColorChip> chips = new List<ColorChip>
+            {
+                new ColorChip(Color.Blue, Color.Yellow),
+                new ColorChip(Color.Red, Color.Green),
+                new ColorChip(Color.Yellow, Color.Red),
+                new ColorChip(Color.Orange, Color.Purple)
+            };
+
+            var securitySystem = new ChipSecuritySystem(chips);
+            List<ColorChip> sequence = securitySystem.FindValidSequence();
+            
+            if (sequence == null)
+            {
+                Console.WriteLine(Constants.ErrorMessage);
+            }
+            else
+            {
+                Console.Write("Blue -> ");
+                foreach (var chip in sequence)
+                {
+                    Console.Write($"[{chip.StartColor}, {chip.EndColor}] -> ");
+                }
+                Console.WriteLine("Green");
+            }
+            Console.ReadKey();
         }
+
     }
 }
